1f7bedaecf86a602bee815b56bb845efb8a7fcae
Aditya Kakaraparti <adk96r@gmail.com>
2023-02-18 20:34:40 +0530

    Remove a space

--- src/Cards/Contact.tsx
+++ src/Cards/Contact.tsx
@@ -16,8 +16,7 @@ export const Contact = (props: CardProps): JSX.Element => {
         
         <div className={`mx-5 mt-4 transition-all delay-500 duration-[800ms] ${active?"opacity-1 translate-y-0":"opacity-0 translate-y-4"}`}>
             Please&nbsp;
-            <Hightlight text={"feel free to reach out"} colors="highlight-red highlight-active text-red-600" delay={"reach-out-highlight"} active={active} />&nbsp;
-            to me if you have any exciting job opportunities available, I'm eager to bring my skills and experience to your team.
+            <Hightlight text={"feel free to reach out"} colors="highlight-red highlight-active text-red-600" delay={"reach-out-highlight"} active={active} />&nbsp;to me if you have any exciting job opportunities available, I'm eager to bring my skills and experience to your team.
         </div>
         <div className={`absolute transition-all delay-[1200ms] duration-[800ms] m-auto right-4 bottom-4 w-fit h-fit flex space-x-2 text-white ${active?"opacity-1 translate-y-0":"opacity-0 translate-y-4 delay-[500ms]"}`}>
             <div className="px-4 py-3 contact-button text-sm font-bold text-center underline rounded-[12px] transition-all hover:scale-[1.1] ">
