62917faf8fb710a5691de0e035fdece6fa335dbd
Aditya <adk96r@gmail.com>
2023-08-29 18:05:19 +0530

    footprint

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,8 +2,10 @@
 import "./style.css";
 import React from "react";
 import {Deck} from "./Deck"
+import {isEqual} from "lodash"
 
 export function App() {
+  console.log(isEqual(1, 2));
   return (
     <div className="relative h-screen w-screen">
       <Deck />
